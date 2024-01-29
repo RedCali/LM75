f3f1ead8798aeba670731dacff4b35caf281a26b
Bastian <mail@bastiangeier.de>
2024-01-29 21:18:44 +0100

    refactoring

--- src/LM75.h
+++ src/LM75.h
@@ -1,9 +1,9 @@
 /*
 * LM75 Temperature Sensor implementation
 * Base   	: Bastian Geier
-* Date      : 30.12.2023
+* Date    : 30.12.2023
 * E-mail	: mail@bastiangeier.de
-* Repo      : https://github.com/RedCali/LM75.git
+* Repo    : https://github.com/RedCali/LM75.git
 
 * Any suggestions are welcome.
 */
@@ -15,9 +15,10 @@
 
 #if defined(ARDUINO) && ARDUINO >= 100
 #pragma message("Compiling for Arduino Framework Architecture...")
-#include "Arduino.h"
-#include <Wire.h>
+#include <Wire.h>S
 #include <avr/io.h>
+
+#include "Arduino.h"
 
 #else
 #pragma message("Compiling for AVR ATMEGA Framework Architecture...")
@@ -46,34 +47,34 @@
 #pragma endregion Constant definition
 
 class LM75 {
- public:
+   public:
 #pragma region PUBLIC
-  explicit LM75(const uint8_t deviceAddress, TwoWire *wire = &Wire);
+    explicit LM75(const uint8_t deviceAddress, TwoWire *wire = &Wire);
 
-  void begin();  // Set address & default pointer -> read temperature
-  void read(void);              // Temperature read
-  int16_t get_i(void);          // Temperature read
-  float get_f(void);            // Temperature read
-  void get_s(char *temp);       // Temperature read
+    void begin();            // Set address & default pointer -> read temperature
+    void read(void);         // Temperature read
+    int16_t get_i(void);     // Temperature read
+    float get_f(void);       // Temperature read
+    void get_s(char *temp);  // Temperature read
 
-  void config_w(uint8_t conf);  // Configuration write
-  uint8_t config_r(void);       // Configuration read
-  void thy_w(uint16_t &thy);    // Temperature hysteresis write
-  uint16_t thy_r(void);         // Temperature hysteresis read
-  void tos_w(uint16_t &tos);    // Temperature over temperature shut down write
-  uint16_t tos_r(void);         // Temperature over temperature shut down read
+    void config_w(uint8_t conf);      // Configuration write
+    uint8_t config_r(void);           // Configuration read
+    void thy_w(const uint16_t &thy);  // Temperature hysteresis write
+    uint16_t thy_r(void);             // Temperature hysteresis read
+    void tos_w(const uint16_t &tos);  // Temperature over temperature shut down write
+    uint16_t tos_r(void);             // Temperature over temperature shut down read
 #pragma endregion PUBLIC
 
- private:
+   private:
 #pragma region PRIVATE
-  TwoWire *_wire;
-  uint8_t _address;
-  uint8_t _config;
-  int16_t _temperature_hyst;
-  int16_t _temperature_os;
-  uint8_t _temperature_high;
-  uint8_t _temperature_low;
-  void temp_rs(uint8_t rs);  // Register select
+    TwoWire *_wire;
+    uint8_t _address = 0;
+    uint8_t _config = 0;
+    int16_t _temperature_hyst = 0;
+    int16_t _temperature_os = 0;
+    uint8_t _temperature_high = 0;
+    uint8_t _temperature_low = 0;
+    void temp_rs(uint8_t rs);  // Register select
 #pragma endregion PRIVATE
 };
 
